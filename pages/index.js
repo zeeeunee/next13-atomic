a7e2768b3b46b4a473098ec1e395f1f448c276e8
zeeeunee <wldmstv@gmail.com>
2024-01-22 16:03:39 +0900

    요리 데이터 출력

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import Head from 'next/head';
 import axios from 'axios';
+import Image from 'next/image';
 
-export default function Home(props) {
-	console.log('ssg', props);
+export default function Home({ meals }) {
+	console.log('ssg', meals);
+	const mealsData = meals.slice(0, 5);
 	return (
 		<>
 			<Head>
 				<title>Main Page</title>
 			</Head>
+
 			<main>
 				<h1>Main page</h1>
+				{mealsData.map((item) => {
+					return (
+						<div key={item.idMeal}>
+							<Image src={item.strMealThumb} alt={item.strMeal} priority width={100} height={100} />
+							<h2>{item.strMeal}</h2>
+						</div>
+					);
+				})}
 			</main>
 		</>
 	);
@@ -17,8 +28,6 @@ export default function Home(props) {
 
 //ssg방식으로 페이지 렌더링
 export async function getStaticProps() {
-	//serialize 에러시 반환받은 propmise객체에서 실제 data값까지 뽑아낸다음에 props로 전달
 	const { data } = await axios.get('/filter.php?c=Seafood');
-	//리턴으로 props객체를 컴포넌트로 전달시 revalidate가 없으면 ssg방식, 있으면 isr방식
 	return { props: data, revalidate: 60 * 60 * 24 };
 }
