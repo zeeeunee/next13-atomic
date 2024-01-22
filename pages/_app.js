ad8d0dda1251dd4212886059c5ac47f5592e10ad
zeeeunee <wldmstv@gmail.com>
2024-01-22 15:09:53 +0900

    next13에서 page기반 라우터 설정방법

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      <Component {...pageProps} />
-    </div>
-  );
+	return (
+		<div>
+			<Component {...pageProps} />
+		</div>
+	);
 }
+/*
+	Next13에서는 page폴더안쪽에 jsx를 넣으면 해당 파일명으로 자동 라우터설정됨 (page router)
+	_document.js (Head영역 설정)
+	_app.js (index.js를 가져와서 document를 적용해서 루트 컴포넌트를 작동시키는 시동파일)
+	index.js (초기 메인페이지 컴포넌트)
+*/
