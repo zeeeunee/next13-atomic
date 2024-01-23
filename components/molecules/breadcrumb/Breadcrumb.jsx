5a10177ef7cc14e1511e1a30a3224ee80808ecdd
zeeeunee <wldmstv@gmail.com>
2024-01-23 15:54:11 +0900

    코드수정

--- components/molecules/breadcrumb/Breadcrumb.jsx
+++ components/molecules/breadcrumb/Breadcrumb.jsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 
 export default function Breadcrumb() {
 	const pathArr = useRouter().asPath.split('/');
+	//인수로 변환할 문자원본과 특수문자를 받아서 특수문자 제거뒤 Capitalize해서 반환하는 함수(화면에 출력될 메뉴명에 활용)
 	const customText = (txt, spc) => {
 		txt = txt.includes(spc)
 			? txt
@@ -15,22 +16,22 @@ export default function Breadcrumb() {
 			: txt;
 		return txt;
 	};
-
 	return (
 		<nav className={clsx(styles.breadcrumb)}>
 			{pathArr.map((name, idx) => {
-				name = customText(name, '-');
+				//displayName = 화면에 출력용도로만 활용되는 텍스트 (해당 값을 로직을 연산하는 부분에 활용금지)
+				const displayName = customText(name, '-');
 				if (idx === pathArr.length - 1) {
 					return (
 						<Text key={idx} tagName={'strong'} isOn>
-							{name}
+							{displayName}
 						</Text>
 					);
 				} else {
 					return (
 						<React.Fragment key={idx}>
 							<Text tagName={'em'} url={'/' + name}>
-								{name === '' ? 'Home' : name}
+								{name === '' ? 'Home' : displayName}
 							</Text>
 							<span> / </span>
 						</React.Fragment>
