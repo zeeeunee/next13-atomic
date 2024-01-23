b84793f05a7c5bf2820418bd0cccb74b8bb26068
zeeeunee <wldmstv@gmail.com>
2024-01-23 15:52:29 +0900

    breadcrumb안쪽에서 텍스트 가공

--- components/molecules/breadcrumb/Breadcrumb.jsx
+++ components/molecules/breadcrumb/Breadcrumb.jsx
@@ -6,23 +6,33 @@ import { useRouter } from 'next/router';
 
 export default function Breadcrumb() {
 	const pathArr = useRouter().asPath.split('/');
+	const customText = (txt, spc) => {
+		txt = txt.includes(spc)
+			? txt
+					.split(spc)
+					.map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+					.join(' ')
+			: txt;
+		return txt;
+	};
 
 	return (
 		<nav className={clsx(styles.breadcrumb)}>
 			{pathArr.map((name, idx) => {
+				name = customText(name, '-');
 				if (idx === pathArr.length - 1) {
 					return (
-						<Text key={idx} tagName={'span'} isOn>
+						<Text key={idx} tagName={'strong'} isOn>
 							{name}
 						</Text>
 					);
 				} else {
 					return (
 						<React.Fragment key={idx}>
-							<Text tagName={'span'} url={'/' + name}>
+							<Text tagName={'em'} url={'/' + name}>
 								{name === '' ? 'Home' : name}
 							</Text>
-							<span>/</span>
+							<span> / </span>
 						</React.Fragment>
 					);
 				}
