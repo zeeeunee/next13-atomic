8a7d5f0b007e9836343ce341274d157dfea87952
zeeeunee <wldmstv@gmail.com>
2024-01-24 16:26:19 +0900

    카테고리 컴포넌트에 데이터 전달

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -1,10 +1,29 @@
 import clsx from 'clsx';
+import axios from 'axios';
 import styles from './find-recipe.module.scss';
+import Category from '@/components/molecules/category/Category';
+import { useState } from 'react';
 
-export default function FindRecipe() {
+export default function FindRecipe({ categories }) {
+	const [Names, setNames] = useState(categories.map((el) => el.strCategory));
+	const [Selected, setSelected] = useState(categories[0].strCategory);
+	console.log(Selected);
+
+	const handleClick = (activeEl) => {
+		setSelected(activeEl);
+	};
+
 	return (
 		<section className={clsx(styles.findRecipe)}>
 			<h1>Find Recipe</h1>
+			<Category dataArr={Names} selected={Selected} onClick={handleClick} />
 		</section>
 	);
+}
+
+export async function getStaticProps() {
+	const { data } = await axios.get(`/categories.php`);
+	return {
+		props: { categories: data.categories },
+	};
 }
