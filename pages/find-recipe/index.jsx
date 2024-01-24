90c0755a9ceb47ed6c8147e35b2de3f721a3dabb
zeeeunee <wldmstv@gmail.com>
2024-01-24 17:08:44 +0900

    목록 데이터 출력

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -3,24 +3,29 @@ import axios from 'axios';
 import styles from './find-recipe.module.scss';
 import Category from '@/components/molecules/category/Category';
 import { useState } from 'react';
+import { useRecipeByCategory } from '@/hooks/useRecipe';
+import Card from '@/components/molecules/card/Card';
 
 export default function FindRecipe({ categories }) {
 	const [Names, setNames] = useState(categories.map((el) => el.strCategory));
 	const [Selected, setSelected] = useState(categories[0].strCategory);
-	console.log(Selected);
+	const { data: dataByCategory, isSuccess } = useRecipeByCategory(Selected, '');
 
 	const handleClick = (activeEl) => {
 		setSelected(activeEl);
 	};
-
 	return (
 		<section className={clsx(styles.findRecipe)}>
 			<h1>Find Recipe</h1>
 			<Category dataArr={Names} selectedEl={Selected} onClick={handleClick} className={clsx(styles.category)} />
+			<h2>{Selected}</h2>
+			{isSuccess &&
+				dataByCategory.map((data) => {
+					return <Card key={data.idMeal} imgSrc={data.strMealThumb} txt={data.strMeal} className={clsx(styles.foodItem)} />;
+				})}
 		</section>
 	);
 }
-
 export async function getStaticProps() {
 	const { data } = await axios.get(`/categories.php`);
 	return {
