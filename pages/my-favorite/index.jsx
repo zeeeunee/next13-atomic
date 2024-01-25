0e92d2c6e521215edb0440dd65f37de7c0887b5a
zeeeunee <wldmstv@gmail.com>
2024-01-25 16:13:50 +0900

    즐겨찾기한 항목 My Favoraite페이지에서 목록으로 출력

--- pages/my-favorite/index.jsx
+++ pages/my-favorite/index.jsx
@@ -1,10 +1,34 @@
 import clsx from 'clsx';
 import styles from './my-favorite.module.scss';
+import { useEffect, useState } from 'react';
+import { useRecipesByIds } from '@/hooks/useRecipe';
+import Card from '@/components/molecules/card/Card';
 
 export default function MyFavorite() {
+	const [SavedId, setSavedId] = useState([]);
+
+	useEffect(() => {
+		setSavedId(JSON.parse(localStorage.getItem('favorite')) || []);
+	}, []);
+
+	const result = useRecipesByIds(SavedId);
+
 	return (
 		<section className={clsx(styles.myFavorite)}>
 			<h1>My favorite</h1>
+			{result.map(({ data, isSuccess }) => {
+				if (isSuccess) {
+					return (
+						<Card
+							key={data.idMeal}
+							imgSrc={data.strMealThumb}
+							txt={data.strMeal}
+							className={styles.favoriteCard}
+							url={`/find-recipe/${data.idMeal}?name=${data.strMeal}`}
+						/>
+					);
+				}
+			})}
 		</section>
 	);
 }
